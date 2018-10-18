19bb6d85f849eea10e6ea69016fcf2999f0fbe0a
Abbi <araldrich9@gmail.com>
2018-10-18 10:37:32 -0500

    fixed method scope and a few typos

--- lib/what_should_i_eat.rb
+++ lib/what_should_i_eat.rb
@@ -3,16 +3,15 @@ require 'net/http'
 require 'json'
 
 module WhatShouldIEat
-  def choose_recipe
-  	result = Net::HTTP.get('www.edamam.com', '/search?type=Feeds')
+  def WhatShouldIEat.choose_recipe
+  	results = Net::HTTP.get('www.edamam.com', '/search?type=Feeds')
   	recipes = JSON.parse(results)
   	recipes.first['items'].sample
   end
 
-  def print_recipe 
-  	random_recipe = choose_recipe
+  def WhatShouldIEat.print_recipe 
+  	random_recipe = WhatShouldIEat.choose_recipe
   	puts "Here's a recipe for you:\n#{random_recipe['label']} \n#{random_recipe['url']}" 
 	end
 
-	print_recipe
 end
